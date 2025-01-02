3ffefc24aade7282db2df596c869c2a658c08c48
Lorenzo <lorenzopica31@gmail.com>
2025-01-02 19:35:47 +0100

    added functionality to add books button

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ const add = document.getElementById("add-books");
 function bookInfo(){
     const sheet = document.createElement("div");
 
-    sheet.style.width = '80px';
-    sheet.style.height = '80px';
+    sheet.style.width = '300px';
+    sheet.style.height = '500px';
 
     sheet.style.backgroundColor = 'white';
+    sheet.style.border = '2px solid black';
+    sheet.style.zIndex = '999';
+    sheet.style.borderRadius = '15px';
+
+    sheet.style.position = 'fixed';
+    sheet.style.top = '50%';
+    sheet.style.left = '50%';
+    sheet.style.transform = 'translate(-50%, -50%)';
+
+    
+    document.body.appendChild(sheet);
 }
 
-add.addEventListener("click", bookInfo());
+add.addEventListener("click", bookInfo);
